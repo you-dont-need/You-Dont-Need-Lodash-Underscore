3ddbc08609ab70d86832a4358c08675624cec67c
Steve Mao <maochenyan@gmail.com>
2017-10-11 12:10:09 +1100

    find and findIndex should not be compatible
    the second parameter could be an object (The `_.matches` iteratee shorthand)

--- tests/index.js
+++ tests/index.js
@@ -8,6 +8,6 @@ assert.equal(plugin.configs['all-warn'].rules['you-dont-need-lodash-underscore/t
 assert.equal(plugin.configs.all.rules['you-dont-need-lodash-underscore/every'], 2);
 assert.equal(plugin.configs.all.rules['you-dont-need-lodash-underscore/keys'], 2);
 assert.equal(plugin.configs['compatible-warn'].rules['you-dont-need-lodash-underscore/each'], undefined);
-assert.equal(plugin.configs['compatible-warn'].rules['you-dont-need-lodash-underscore/find-index'], 1);
+assert.equal(plugin.configs['compatible-warn'].rules['you-dont-need-lodash-underscore/last-index-of'], 1);
 assert.equal(plugin.configs.compatible.rules['you-dont-need-lodash-underscore/for-each'], 1);
 assert.equal(plugin.configs.compatible.rules['you-dont-need-lodash-underscore/is-nan'], 2);

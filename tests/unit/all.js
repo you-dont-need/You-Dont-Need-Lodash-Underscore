67076a5d54bafadce10d95b51c0fc037ab500f41
inuscript <inuscript@users.noreply.github.com>
2017-12-29 14:06:52 +0900

    testing with mocha

--- tests/unit/all.js
+++ tests/unit/all.js
@@ -2,48 +2,45 @@
 const assert = require('assert');
 const _ = require('lodash');
 
-let tests = {}
+describe('code snippet example', () => {
 
-tests.compact = function(){
-  assert.deepEqual(
-    _.compact([0, 1, false, 2, '', 3]),
-    [0, 1, false, 2, '', 3].filter(v => v)
-  )
-}
+  it('compact', () => {
+    assert.deepEqual(
+      _.compact([0, 1, false, 2, '', 3]),
+      [0, 1, false, 2, '', 3].filter(v => v)
+    )
+  })
 
-tests.concat = function() {
-  const lodashArray = [1]
-  const lodashResult = _.concat(lodashArray, 2, [3], [[4]])
+  it('concat', () => {
+    const lodashArray = [1]
+    const lodashResult = _.concat(lodashArray, 2, [3], [[4]])
 
-  const nativehArray = [1]
-  const nativeResult = nativehArray.concat(2, [3], [[4]])
+    const nativehArray = [1]
+    const nativeResult = nativehArray.concat(2, [3], [[4]])
 
-  assert.deepEqual(lodashResult, nativeResult)
-}
+    assert.deepEqual(lodashResult, nativeResult)
+  })
 
-tests.fill1 = function() {
-  var array = [1, 2, 3]
-  assert.deepEqual(
-    _.fill(array, 'a'),
-    array.fill('a')
-  )
-}
+  describe('fill', () => {
+    it("_.fill(array, 'a')", () => {
+      var array = [1, 2, 3]
+      assert.deepEqual(
+        _.fill(array, 'a'),
+        array.fill('a')
+      )
+    })
+    it("_.fill(Array(3), 2)", () => {
+      assert.deepEqual(
+        _.fill(Array(3), 2),
+        Array(3).fill(2)
+      )
+    })
 
-tests.fill2 = function() {
-  assert.deepEqual(
-    _.fill(Array(3), 2),
-    Array(3).fill(2)
-  )
-}
-
-tests.fill3 = function() {
-  assert.deepEqual(
-    _.fill([4, 6, 8, 10], '*', 1, 3),
-    [4, 6, 8, 10].fill('*', 1, 3)
-  )
-}
-
-// Run tests
-Object.values(tests).forEach((testFn]) => {
-  testFn()
+    it("_.fill([4, 6, 8, 10], '*', 1, 3)", () => {
+      assert.deepEqual(
+        _.fill([4, 6, 8, 10], '*', 1, 3),
+        [4, 6, 8, 10].fill('*', 1, 3)
+      )
+    })
+  })
 })
